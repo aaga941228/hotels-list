ad0dd75737317199f7dda2a2d8ded695b126c6ba
Alan Ayala <aaga941228@outlook.es>
2022-05-11 13:14:47 -0500

    hotels service and list added

---
+++ src/assets/hotel-list.ts
@@ -0,0 +1,71 @@
+export const hotels = [
+  {
+    name: 'Holiday Inn French Quarter-Chateau LeMoyne',
+    code: 'MSYCL',
+    contact: '1-504-5811303',
+    address: {
+      city: 'New Orleans',
+      country: {
+        code: 'US',
+        name: 'United States',
+      },
+      state: {
+        code: 'LA',
+        name: 'Louisiana',
+      },
+      zip: '70112',
+      street1: '301  Dauphine Street',
+    },
+    description:
+      'Guests love the Chateau LeMoyne in New Orleans. Our boutique hotel has a wonderful location in the French Quarter. A beautiful setting combined with historic architecture and modern amenities, offers travelers the charm and culture of Louisiana.',
+    price: 200,
+    currency: 'USD',
+  },
+  {
+    name: 'Crowne Plaza New Orleans French Quarter',
+    code: 'MSYLA',
+    contact: '1-504-9620500',
+    address: {
+      city: 'New Orleans',
+      country: {
+        code: 'US',
+        name: 'United States',
+      },
+      state: {
+        code: 'LA',
+        name: 'Louisiana',
+      },
+      zip: '70130',
+      street1: '739 Canal St. @ Bourbon',
+    },
+    description:
+      'Delivering high quality accommodations in New Orleans, the Crowne Plaza New Orleans French Quarter offers a variety of accommodations that will make your stay even better. Optimal productivity is a priority here with the 32,000 sq ft of meeting space and an on-site Business Center. Our staff will handle your business needs so you can take it easy in one of our 693 guest rooms, including 32 suites.',
+    price: 161,
+    currency: 'USD',
+  },
+  {
+    name: 'Hotel Indigo New Orleans - French Quarter',
+    code: 'MSYRL',
+    contact: '1504-504-5955605',
+    address: {
+      city: 'New Orleans',
+      country: {
+        code: 'US',
+        name: 'United States',
+      },
+      state: {
+        code: 'LA',
+        name: 'Louisiana',
+      },
+      zip: '70130',
+      street1: '705 Common Street',
+      street4: null,
+      consumerFriendlyURL: 'www.hotelindigo.com/frenchquarter',
+      facebookUrl: 'www.facebook.com/hotelindigofrenchquarter',
+    },
+    description:
+      'Taking inspiration from the booming melodies of the brass band, the creative costuming of citizens and visitors alike, and the enticing beckon of second line parades, the guest at the Hotel Indigo New Orleans will be “part of that number”. With 143 perfectly appointed guestrooms, nine with balconies, and a lively lounge, our pet friendly hotel is the perfect oasis in the heart of New Orleans. ',
+    price: 175,
+    currency: 'USD',
+  },
+];
